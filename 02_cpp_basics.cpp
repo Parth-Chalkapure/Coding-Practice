9523d642865a1a56db0334541e4d1bb9796c865e
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-15 10:56:26 +0530

    Added Data Types practice code

---
+++ 02_cpp_basics.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // int, long, long long, float, double
+    // string and getline
+    // char
+    char ch = 'c';
+    cout << "character is " << ch;
+    return 0;   
+}
