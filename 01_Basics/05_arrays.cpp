7d2ff024560e0e64ea172c9483c43b936ef555cf
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-16 10:36:14 +0530

    added arrays practice code

---
+++ 01_Basics/05_arrays.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float myarr[5];
+    cin >> myarr[0] >> myarr[1] >> myarr[2] >> myarr[3] >> myarr[4];
+    cout << myarr[4];
+    return 0;
+}
+
+/* === 2D Array ===
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[3][5];
+    arr[3][5] = 1234;
+    cout << arr[1][4]; // this gives any random garbage value aside from arr[3][5]
+    return 0;
+}
+*/
