427fc6f260e9f0765b86fa794d9388d288aade33
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-17 14:38:35 +0530

    added while loops practice code

---
+++ 01_Basics/08_while_loops.cpp
@@ -0,0 +1,27 @@
+/* #include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    cin >> i;
+    int j = 1;
+    while(j <= 10)  {
+        cout << i*j << endl;
+        j = j + 1;
+    }
+    return 0;
+} */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i = 3;
+    do {
+        cout << "E_ROR_" << 3 << endl;
+        i = i + 1;
+    } while(i <= 3);
+    cout << i;
+    return 0;
+}
+
