e90157837c4d1e219af3af55fe5db9dfdc6723ed
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-17 14:26:59 +0530

    added for loops practice code

---
+++ 01_Basics/07_for_loops.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    int j;
+    cin >> i;
+    for(j = 1; j <= 10; j = j + 1) {
+        cout << i*j << endl;
+    }
+    return 0;
+}
