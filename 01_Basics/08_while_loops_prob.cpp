633feb91afa3ef6abc6d6b168eb57b8f92c0df54
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-19 21:41:29 +0530

    added while loop problem solution [TUF]

--- 01_Basics/08_while_loops_prob.cpp
+++ 01_Basics/08_while_loops_prob.cpp
@@ -1,8 +1,11 @@
 /* Q. Given a digit d (0 to 9), find the sum of the first 50 positive integers (integers > 0) that end with digit d.
 A number ends with digit d if its last digit is d. */
 
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
-    public:
+public:
     int whileLoop(int d) {
         int x = 0;
         int y = 0;
@@ -13,3 +16,16 @@ class Solution {
         return y;
     }
 };
+
+int main() {
+    Solution solver;
+    
+    // Testing the function with a sample value for 'd'
+    int test_d = 5;
+    int result = solver.whileLoop(test_d);
+    
+    // Printing the output to your terminal
+    cout << "The result for d = " << test_d << " is: " << result << endl;
+    
+    return 0;
+}
