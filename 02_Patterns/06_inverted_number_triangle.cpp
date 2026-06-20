717743bdb6a1397503058b3abd93aabbbe91b9e1
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-20 11:36:43 +0530

    added first six pattern problems

---
+++ 02_Patterns/06_inverted_number_triangle.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    for(n; n > 0; n--) {
+        for(int j = 0; j < n; j++) {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
