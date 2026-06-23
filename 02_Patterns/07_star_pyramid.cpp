f91d127737614f14b94255e40e8272f201c74343
Parth Chalkapure <parthchalkapure@gmail.com>
2026-06-23 14:40:34 +0530

    updated star pyramid code

--- 02_Patterns/07_star_pyramid.cpp
+++ 02_Patterns/07_star_pyramid.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* === First Try ===
 void printPattern(int n) { 
     for(int i = 0, m = n * 2 - 2; i < n; i++, m -= 2) {
         for(int o = m; o > 0; o--) {
@@ -11,6 +12,19 @@ void printPattern(int n) {
         }
         cout << endl;
     }
+} */
+
+void printPattern(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < 2*n - 2*i - 2; j++) {
+            cout << " ";
+        }
+        for(int k = 0; k < 2*i + 1; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
 }
 
 int main() {
