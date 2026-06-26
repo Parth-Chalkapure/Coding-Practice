5a1e48ebb4744e221abc4b46d41b4dc2c5504577
Parth <parthchalkapure@gmail.com>
2026-06-26 22:15:51 +0530

    added 14 to 16 pattern problems

---
+++ 02_Patterns/15_reverse_letter_triangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i; j++) {
+            cout << s[j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
+
