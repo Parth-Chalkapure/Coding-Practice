1ec9ccb728f385a3f7b63e5b01707e1c9f683261
Parth <parthchalkapure@gmail.com>
2026-06-30 11:16:45 +0530

    added pairs STL practice code

---
+++ 03_STL/01_pairs.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void understandPairs() {
+    // pair<int, int> p = {2, 3};
+    // cout << p.second << " " << p.first;
+
+    // pair<int, pair<int, int>> p = {2, {4, 5}};
+    // cout << p.second.first << " " << p.first << " " << p.second.second;
+
+    // pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+    // cout << arr[2].second; 
+
+    pair<string, string> p = {"Ram", "Sham"};
+    cout << p.first;
+}
+
+int main() {
+    understandPairs();
+    return 0;
+}
+
